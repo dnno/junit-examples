1da0410f491fa600de23472908a1e980a395c01b
Reinhard Prechtl <reinhard.prechtl@codecentric.de>
2016-12-02 15:59:11 +0100

    Demo tests for combinatorial use of parameter values

--- src/test/java/de/rpr/junit/theories/TheoryTests.java
+++ src/test/java/de/rpr/junit/theories/TheoryTests.java
@@ -19,6 +19,24 @@ public class TheoryTests {
     assertTrue(new Validator().validate(number));
   }
 
+  // Theories runner will invoke test with all combinations of the two parameters.
+  @Theory
+  public void linearTestWithMultipleParameters(
+      @IntegerInRange(to = 5) int left,
+      @IntegerInRange(to = 2) int right) {
+    assertTrue(new Validator().validate(left));
+    assertTrue(new Validator().validate(right));
+  }
+
+  // Theories runner will invoke test with all combinations of the two parameters.
+  @Theory
+  public void randomTestWithMultipleParameters(
+      @RandomInteger(numValues = 10, upperBound = 4) int left,
+      @RandomInteger(numValues = 10, upperBound = 8) int right) {
+    assertTrue(new Validator().validate(left));
+    assertTrue(new Validator().validate(right));
+  }
+
   class Validator {
 
     boolean validate(int value) {
